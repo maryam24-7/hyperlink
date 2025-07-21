95378b573fce027898e970e61a99bae8cd85716a
Maryam Alrefai <mmm792792@gmail.com>
2025-07-22 00:32:25 +0300

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  reactStrictMode: true,
+  images: {
+    domains: ['www.google-analytics.com'],
+  },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN',
+          },
+        ],
+      },
+    ];
+  },
+};
