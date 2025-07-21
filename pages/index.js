9e0f5145ee1a24391b44e54830fb49739347d75e
Maryam Alrefai <mmm792792@gmail.com>
2025-07-22 00:28:55 +0300

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import { useState } from 'react';
+import Layout from '../components/Layout';
+import FileUpload from '../components/FileUpload';
+import ResultsDisplay from '../components/ResultsDisplay';
+
+export default function Home() {
+  const [results, setResults] = useState(null);
+
+  return (
+    <Layout>
+      <h1>Resume ATS Checker</h1>
+      <p>Upload your resume to check ATS compatibility</p>
+      <FileUpload onAnalysisComplete={setResults} />
+      {results && <ResultsDisplay results={results} />}
+    </Layout>
+  );
+}
