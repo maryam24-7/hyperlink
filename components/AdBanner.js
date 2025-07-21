11b3a93e2be5f9269458b5bfc0212b920d86545f
Maryam Alrefai <mmm792792@gmail.com>
2025-07-22 01:19:42 +0300

    Create AdBanner.js

---
+++ components/AdBanner.js
@@ -0,0 +1,10 @@
+export default function AdBanner() {
+  return (
+    <div className="ad-container">
+      {/* هنا يمكنك إضافة كود إعلاناتك */}
+      <div style={{ height: '90px', background: '#f0f0f0', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+        Ad Banner Space
+      </div>
+    </div>
+  );
+}
