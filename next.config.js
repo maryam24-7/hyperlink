04111b1b3d6a0cf6a945c1687a6f197a87bd5f48
Maryam Alrefai <mmm792792@gmail.com>
2025-07-23 23:07:12 +0300

    Delete next.config.js

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  // Enable static exports for Vercel
-  output: 'standalone',
-}
-
-module.exports = nextConfig
