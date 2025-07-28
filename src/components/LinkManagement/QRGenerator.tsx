24e8c58284100973aecca7b84b198b87000711d6
Maryam Alrefai <mmm792792@gmail.com>
2025-07-29 02:43:30 +0300

    Update QRGenerator.tsx

--- src/components/LinkManagement/QRGenerator.tsx
+++ src/components/LinkManagement/QRGenerator.tsx
@@ -3,7 +3,7 @@ import QRCode from 'qrcode.react';
 export default function QRGenerator({ url, size = 200 }) {
   if (!url) return null;
   return (
-    <div style={{ margin: "16px 0", display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+    <div style={{ margin: "16px 0", textAlign: "center" }}>
       <QRCode value={url} size={size} fgColor="#4f46e5" bgColor="#fff" />
     </div>
   );
