962e6dcac294551e96cce274eab38cf9c590914b
Maryam Alrefai <mmm792792@gmail.com>
2025-07-29 02:39:45 +0300

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import LinkForm from "../components/LinkManagement/LinkForm";
+import QRForm from "../components/LinkManagement/QRForm";
 import Link from "next/link";
 import { FaChartBar, FaQrcode } from "react-icons/fa";
 
@@ -14,6 +15,7 @@ export default function Home() {
       </header>
       <main>
         <LinkForm />
+        <QRForm />
         <div style={{ margin: "32px 0", textAlign: "center" }}>
           <Link href="/dashboard">
             <button style={{ marginRight: 12 }}>
